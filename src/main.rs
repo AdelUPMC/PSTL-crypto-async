a0b2b6ad6dc7faa50ea887b1b011750b3cd9d44a
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2019-02-03 22:07:01 +0100

    Rewrite of orthogonalization fonction from C to Rust

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,42 @@ fn main() {
     say(out, width, &mut writer).unwrap();
 }
 */
+
 use std::io;
 use rand::prelude::*;
+
+static  mask_l:[u64;6]=[0xaaaaaaaaaaaaaaaa,
+	0xcccccccccccccccc,
+	0xf0f0f0f0f0f0f0f0,
+	0xff00ff00ff00ff00,
+	0xffff0000ffff0000,
+	0xffffffff00000000];
+
+static  mask_r:[u64;6]=[0x5555555555555555,
+	0x3333333333333333,
+	0x0f0f0f0f0f0f0f0f,
+	0x00ff00ff00ff00ff,
+	0x0000ffff0000ffff,
+	0x00000000ffffffff];
+
+fn real_ortho(&mut data:[u64;64]){
+	for i in 0..6{
+		let n:u64=(1 as u64)<<i;
+		let mut j:u64=0;
+		while j<64{
+			for k in 0..n{
+				let u:u64= data[j+k] & mask_l[i];
+				let v:u64= data[j+k] & mask_r[i];
+				let x:u64= data[j+n+k] & mask_l[i];
+				let y:u64= data[j+n+k] & mask_r[i];
+				data[j + k] = u | (x >> n);
+				data[j + n + k] = (v << n) | y;
+			}
+			j+=2*n;
+		}
+
+	} 
+}
 
 fn main() {
     println!("Guess the number!");
@@ -30,3 +64,8 @@ fn main() {
     println!("You guessed: {}", guess);
 	println!("generated number: {}",(n as u64));
 }
+
+
+
+
+
