0c7d2763d545a602a006f45b2ef89295f9911716
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2019-04-01 22:39:43 +0200

    added function transform_u64_to_array_of_u8

--- sandbox/client/src/main.rs
+++ sandbox/client/src/main.rs
@@ -11,6 +11,18 @@ use rand::prelude::*;
 use time::PreciseTime;
 
 
+fn transform_u64_to_array_of_u8(x: u64) -> [u8; 8] {
+    let b1: u8 = ((x >> 56) & 0xff) as u8;
+    let b2: u8 = ((x >> 48) & 0xff) as u8;
+    let b3: u8 = ((x >> 40) & 0xff) as u8;
+    let b4: u8 = ((x >> 32) & 0xff) as u8;
+    let b5: u8 = ((x >> 24) & 0xff) as u8;
+    let b6: u8 = ((x >> 16) & 0xff) as u8;
+    let b7: u8 = ((x >> 8) & 0xff) as u8;
+    let b8: u8 = (x & 0xff) as u8;
+    return [b1, b2, b3, b4, b5, b6, b7, b8];
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let nbrequest: usize = args[1].parse().unwrap();
@@ -23,14 +35,18 @@ fn main() {
 
             //write
             let rand_u64: u64 = rand::thread_rng().gen();
+			let array:[u8;8]=transform_u64_to_array_of_u8(rand_u64);
 
-            let request: String = rand_u64.to_string();
-            stream.write(request.as_bytes()).unwrap();
+            //let request: String = rand_u64.to_string();
+            //stream.write(request.as_bytes()).unwrap();
+			stream.write(&array).unwrap();
 
             //read
-            let mut buffer = [0; 512];
+            let mut buffer:[u8;8] = [0;8];
             stream.read(&mut buffer).unwrap();
-            println!("Received : {}", String::from_utf8_lossy(&buffer));
+			let plain= u64::from_be_bytes(buffer);
+            //println!("Received : {}", String::from_utf8_lossy(&buffer));
+			println!("Received : {}",plain);
         });
         vec_thread.push(handle);
     }
